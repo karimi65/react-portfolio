2248bb263e4fddb08f080e051e55bd7aafabbbc6
Captain <71038018+karimi65@users.noreply.github.com>
2021-04-06 13:23:29 -0500

    Add columns to Home-page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,30 @@ const Home = () => {
     return (
         <div className='container'>
             <h1>Home</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                        <h2>
+                            About Me
+                        </h2>
+                    </div>
+                    <div className="col-6">
+                        <img />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6">
+                        <h2>
+                            Skills:
+                        </h2>
+                    </div>
+                    <div className="col-6">
+                        <h3>
+                            Hobbies:
+                        </h3>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
