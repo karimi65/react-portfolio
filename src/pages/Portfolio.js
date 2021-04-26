067d8f0edf22550c2d4e38b2ae08a4f8a2c815b3
Captain <71038018+karimi65@users.noreply.github.com>
2021-04-26 11:41:07 -0500

    deployment

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,7 +6,7 @@ function Portfolio() {
         <div className="container">
             <h1>portfolio</h1>
             <div className="row">
-                <Card image alt="project1" name="work day" repo="https://github.com/karimi65/work-day-scheduler" web="https://karimi65.github.io/work-day-scheduler/" />
+                <Card image="/img/project1.JPG" alt="project1" name="work day" repo="https://github.com/karimi65/work-day-scheduler" web="https://karimi65.github.io/work-day-scheduler/" />
                 <Card image alt="project2" name="wikigames" repo="https://github.com/mokulling/wikigames" web="https://mokulling.github.io/wikigames/" about="dfhsfhfhfhfhfh" />
                 <Card image alt="project3" name="burger" repo="https://github.com/karimi65/burger" web="https://enigmatic-waters-41603.herokuapp.com/" />
             </div>
